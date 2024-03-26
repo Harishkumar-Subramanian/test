fe81e4670ac7906c9b671384a394ab10a349262a
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-26 15:35:48 +0530

    Add host rule in config file

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: ['config:recommended'],
-  repositories: ['Harishkumar-Subramanian/test']
+  repositories: ['Harishkumar-Subramanian/test'],
+  hostRules: [
+    {
+      hostType: 'docker',
+      matchHost: 'trimbletransportationcloud.azurecr.io',
+      username: 'global-pull-token',
+      password: process.env.OCI_REGISTRY_PASSWORD
+    }
+  ]
 };
