5ab2b419c48d85878e5dbd86c8f21b66eb87efa2
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-26 12:57:58 +0530

    update config

--- config.js
+++ config.js
@@ -1,20 +1,4 @@
 module.exports = {
   extends: ['config:recommended'],
-  packageRules: [
-    {
-      matchPackageNames: ['helm/ttc-standard-app'],
-      matchDatasources: ['docker']
-    }
-  ],
-  customManagers: [
-    {
-      customType: 'regex',
-      fileMatch: ['^apps/.*/.*/values\\.yaml$'],
-      matchStrings: ['^helm:\\s*targetRevision:\\s*(?<currentValue>.*)$'],
-      depNameTemplate: 'helm/ttc-standard-app',
-      datasourceTemplate: 'docker',
-      versioningTemplate: '^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)$'
-    }
-  ],
   repositories: ['Harishkumar-Subramanian/test']
 };
