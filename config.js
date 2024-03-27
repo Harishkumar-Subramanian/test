8994841ce234aa17da4c1b577af58871690f73db
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-27 15:24:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,21 @@
 module.exports = {
   repositories: ['Harishkumar-Subramanian/test'],
+  customManagers: [
+    {
+      customType: 'regex',
+      fileMatch: ['values.yaml'],
+      matchStrings: ['(?<currentValue>helm:\\s*targetRevision:\\s*)(?<version>\\S+)'],
+      depNameTemplate: 'helm/ttc-standard-app',
+      versioningTemplate: 'semver-coerced',
+      datasourceTemplate: 'custom.helm',
+      currentValueTemplate: '{{version}}'
+    }
+  ],
+   customDatasources: {
+    helm: {
+      defaultRegistryUrlTemplate: 'https://trimbletransportationcloud/helm/ttc-standard-app'
+    }
+  },
   hostRules: [
     {
       hostType: 'helm',
