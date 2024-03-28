e42e52d98090e0b8859a10ef329768d23ba35b19
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-28 15:03:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
       customType: 'regex',
       fileMatch: ['values.yaml'],
       matchStrings: ['(?<currentValue>helm:\\s*targetRevision:\\s*)(?<version>\\S+)'],
+      depNameTemplate: 'tcxcontainers.azurecr.io',
       registryUrlTemplate: 'https://tcxcontainers.azurecr.io',
       versioningTemplate: 'semver-coerced',
       datasourceTemplate: 'helm',
