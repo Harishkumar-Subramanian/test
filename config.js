e12cedab42b25cf15c08a939a1b3eff359d2055c
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-28 14:27:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,18 +11,15 @@ module.exports = {
       currentValueTemplate: '{{version}}'
     }
   ],
-  packageRules: [
+  registryAliases: {
+    stable: 'https://tcxcontainers.azurecr.io/helm/ttc-standard-app'
+  },
+  hostRules: [
     {
-      packagePatterns: ['helm:'],
-      datasource: 'helm',
-      registryUrls: ['https://tcxcontainers.azurecr.io'],
-      versioning: 'semver-coerced',
-      authentication: {
-        type: 'servicePrincipal',
-        clientId: 'c5db4df2-bd84-4606-b590-e6873154cd17',
-        clientSecret: process.env.RENOVATE_AZURE_REGISTRY_PASSWORD,
-        tenantId: '23235593-4c46-41c1-944b-32a03aab96e6'
-      }
+      matchHost: 'https://tcxcontainers.azurecr.io/helm/ttc-standard-app',
+      hostType: 'docker',
+      username: 'tcxcontainers',
+      password: process.env.RENOVATE_AZURE_REGISTRY_PASSWORD
     }
   ]
 };
