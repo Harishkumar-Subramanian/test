10157471911e33789ecc26497d4e01e2deedaa01
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-28 13:28:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,13 @@ module.exports = {
       packagePatterns: ['helm:'],
       datasource: 'helm',
       registryUrls: ['https://tcxcontainers.azurecr.io'],
-      versioning: 'semver-coerced'
+      versioning: 'semver-coerced',
+      authentication: {
+        type: 'servicePrincipal',
+        clientId: 'c5db4df2-bd84-4606-b590-e6873154cd17',
+        clientSecret: process.env.RENOVATE_AZURE_REGISTRY_PASSWORD,
+        tenantId: '23235593-4c46-41c1-944b-32a03aab96e6'
+      }
     }
   ]
 };
