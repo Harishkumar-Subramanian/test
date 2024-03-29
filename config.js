5333b99d835b041c4ad69416c1508a6759505cb8
Harishkumar-Subramanian <127851488+Harishkumar-Subramanian@users.noreply.github.com>
2024-03-29 12:41:32 +0530

    test renovate

--- config.js
+++ config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  repositories: ['Harishkumar-Subramanian/test'],
-  customManagers: [
-    {
-      customType: 'regex',
-      fileMatch: ['values.yaml'],
-      matchStrings: ['(?<currentValue>helm:\\s*targetRevision:\\s*)(?<version>\\S+)'],
-      depNameTemplate: 'helm/ttc-standard-app',
-      registryUrlTemplate: 'https://trimbletransportationcloud.azurecr.io',
-      versioningTemplate: 'semver-coerced',
-      datasourceTemplate: 'docker',
-      currentValueTemplate: '{{version}}'
-    }
-  ],
-  packageRules: [
-    {
-      matchDatasources: ['docker'],
-      matchPackageNames: ['helm/ttc-standard-app'],
-      versioning: 'semver-coerced'
-    }
-  ],
-  hostRules: [
-    {
-      matchHost: 'https://trimbletransportationcloud.azurecr.io',
-      username: process.env.RENOVATE_AZURE_REGISTRY_USERNAME,
-      password: process.env.RENOVATE_AZURE_REGISTRY_PASSWORD
-    }
-  ]
-};
+    repositories: ['Harishkumar-Subramanian/test'],
+    customManagers: [
+      {
+        customType: 'regex',
+        fileMatch: ['values.yaml'],
+        matchStrings: ['(?<currentValue>helm:\\s*targetRevision:\\s*)(?<version>\\S+)'],
+        depNameTemplate: 'helm/ttc-standard-app',
+        registryUrlTemplate: 'https://trimbletransportationcloud.azurecr.io',
+        versioningTemplate: 'semver-coerced',
+        datasourceTemplate: 'docker',
+        currentValueTemplate: '{{version}}'
+      }
+    ],
+    packageRules: [
+      {
+        matchDatasources: ['docker'],
+        matchPackageNames: ['helm/ttc-standard-app'],
+        versioning: 'semver-coerced'
+      }
+    ],
+    hostRules: [
+      {
+        matchHost: 'https://trimbletransportationcloud.azurecr.io',
+        username: process.env.OCI_REGISTRY_USERNAME,
+        password: process.env.OCI_REGISTRY_PASSWORD
+      }
+    ]
+  };
+  
